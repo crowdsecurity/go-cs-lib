74267b576df81d8928b07e9e56b1477d41f41c05
Marco Mariani <marco@crowdsec.net>
2023-06-05 11:11:46 +0200

    test Chunks with size=0 (keep whole slice)

--- pkg/slicetools/chunk_test.go
+++ pkg/slicetools/chunk_test.go
@@ -1,32 +1,34 @@
 package slicetools_test
 
 import (
-	"reflect"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/crowdsecurity/go-cs-lib/pkg/slicetools"
 )
 
 func TestChunks(t *testing.T) {
 	testCases := []struct {
+		name      string
 		items     []int
 		chunkSize int
 		expected  [][]int
 	}{
-		{[]int{}, 2, [][]int{}},
-		{[]int{1}, 2, [][]int{{1}}},
-		{[]int{1, 2, 3, 4, 5}, 2, [][]int{{1, 2}, {3, 4}, {5}}},
-		{[]int{1, 2, 3, 4, 5}, 3, [][]int{{1, 2, 3}, {4, 5}}},
-		{[]int{1, 2, 3, 4, 5}, 5, [][]int{{1, 2, 3, 4, 5}}},
-		{[]int{1, 2, 3, 4, 5}, 6, [][]int{{1, 2, 3, 4, 5}}},
+		{ "empty slice, chunk size 2", []int{}, 2, [][]int{}},
+		{"1 element, chunk size 2",    []int{1}, 2, [][]int{{1}}},
+		{"empty slice, chunk size 0",  []int{}, 0, [][]int{}},
+		{"5 elements, chunk size 2",   []int{1, 2, 3, 4, 5}, 2, [][]int{{1, 2}, {3, 4}, {5}}},
+		{"5 elements, chunk size 3",   []int{1, 2, 3, 4, 5}, 3, [][]int{{1, 2, 3}, {4, 5}}},
+		{"5 elements, chunk size 4",   []int{1, 2, 3, 4, 5}, 5, [][]int{{1, 2, 3, 4, 5}}},
+		{"5 elements, chunk size 6",   []int{1, 2, 3, 4, 5}, 6, [][]int{{1, 2, 3, 4, 5}}},
+		{"chunk size 0 = don't chunk", []int{1, 2, 3, 4, 5}, 0, [][]int{{1, 2, 3, 4, 5}}},
+		{"look ma, no sorting",        []int{1, 2, 4, 1, 5}, 2, [][]int{{1, 2}, {4, 1}, {5}}},
 	}
 
 	for _, tc := range testCases {
-		t.Run("", func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			actual := slicetools.Chunks(tc.items, tc.chunkSize)
-			if !reflect.DeepEqual(actual, tc.expected) {
-				t.Errorf("Expected %v, but got %v", tc.expected, actual)
-			}
+			assert.Equal(t, tc.expected, actual)
 		})
 	}
 }
