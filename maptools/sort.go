da46b8bbaa8878184a47f87303f338beb11ef360
bui <thibault@crowdsec.net>
2024-01-11 15:25:01 +0100

    naming

--- maptools/sort.go
+++ maptools/sort.go
@@ -2,7 +2,7 @@ package maptools
 
 import "sort"
 
-func sortedMapKeys[V any](m map[string]V) []string {
+func SortedKeys[V any](m map[string]V) []string {
 	keys := make([]string, 0, len(m))
 	for k := range m {
 		keys = append(keys, k)
