2823f38b9b6cf5d7e7446d6a830b536a0c2b66d2
Thibault "bui" Koechlin <orixxx@gmail.com>
2024-01-11 15:48:52 +0100

    move generic code from pkg/hubtest (#10)

---
+++ maptools/sort.go
@@ -0,0 +1,14 @@
+package maptools
+
+import "sort"
+
+func SortedKeys[V any](m map[string]V) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	sort.Strings(keys)
+
+	return keys
+}
