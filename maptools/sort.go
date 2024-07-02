7bba11eb2eab9adbedc1239a0d8c7fd3a6fbdaaa
mmetc <92726601+mmetc@users.noreply.github.com>
2024-07-02 14:29:21 +0200

    maptools/SortedKeysNoCase() (#17)

--- maptools/sort.go
+++ maptools/sort.go
@@ -1,6 +1,9 @@
 package maptools
 
-import "sort"
+import (
+	"sort"
+	"strings"
+)
 
 func SortedKeys[V any](m map[string]V) []string {
 	keys := make([]string, 0, len(m))
@@ -9,6 +12,25 @@ func SortedKeys[V any](m map[string]V) []string {
 	}
 
 	sort.Strings(keys)
+
+	return keys
+}
+
+func SortedKeysNoCase[V any](m map[string]V) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		li, lj := strings.ToLower(keys[i]), strings.ToLower(keys[j])
+		if li == lj {
+			// differ only by case, sort by original key
+			// will have uppercase first
+			return keys[i] < keys[j]
+		}
+		return li < lj
+	})
 
 	return keys
 }
