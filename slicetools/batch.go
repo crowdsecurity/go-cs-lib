4d178cf38b1169b7c47fedc38cd28709143645a5
marco <marco@crowdsec.net>
2025-09-23 12:48:35 +0200

    wip

--- slicetools/batch.go
+++ slicetools/batch.go
@@ -1,36 +1,53 @@
 package slicetools
 
-import "context"
+import (
+	"context"
+	"slices"
+)
 
-// a simple alternative to slices.Chunk (or slicetools.Chunks)
-// if you want no allocations, context cancelation and no parallelism
+// a simple wrapper around slices.Chunk
+// if you want context cancelation and don't need parallelism
 //
-// also: doesn't panic for batchSize = 0
+// also: doesn't panic for size = 0
 
-// Batch applies fn to successive chunks of elements, each of size at most batchSize.
-// A batchSize of 0 (or negative) processes all the elements in one chunk.
+// Batch applies fn to successive chunks of at most "size" elements.
+// A size of 0 (or negative) processes all the elements in one chunk.
 // Stops at the first error and returns it.
-func Batch[T any](ctx context.Context, elems []T, batchSize int, fn func(context.Context, []T) error) error {
+func Batch[T any](ctx context.Context, elems []T, size int, fn func(context.Context, []T) error) error {
 	n := len(elems)
 
 	if n == 0 {
 		return nil
 	}
 
-	if batchSize <= 0 || batchSize > n {
-		batchSize = n
+	if size <= 0 || size > n {
+		size = n
 	}
 
-	for start := 0; start < n; start += batchSize {
-		if ctx.Err() != nil {
-			return ctx.Err()
+	// delegate to stdlib
+
+	for part := range slices.Chunk(elems, size) {
+		if err := ctx.Err(); err != nil {
+			return err
 		}
 
-		end := min(start+batchSize, n)
-		if err := fn(ctx, elems[start:end]); err != nil {
+		if err := fn(ctx, part); err != nil {
 			return err
 		}
 	}
+
+	// we have stdlib at home
+
+	//	for start := 0; start < n; start += size {
+	//		if ctx.Err() != nil {
+	//			return ctx.Err()
+	//		}
+	//
+	//		end := min(start+size, n)
+	//		if err := fn(ctx, elems[start:end]); err != nil {
+	//			return err
+	//		}
+	//	}
 
 	return nil
 }
